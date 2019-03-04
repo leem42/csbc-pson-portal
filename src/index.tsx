f7963b0409d2f3e03c8b4f2120067197948a8bb6
Michael Lee <michael.leem42@gmail.com>
2019-03-04 14:47:23 -0800

    changed tsconfig to use react preset

--- src/index.tsx
+++ src/index.tsx
@@ -2,9 +2,10 @@ import * as React from 'react'
 // tslint:disable-next-line:import-name
 import ReactDOM from 'react-dom'
 import App from './App'
+import * as serviceWorker from './serviceWorker'
 ReactDOM.render(<App />, document.getElementById('root'))
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
-// serviceWorker.unregister()
+serviceWorker.unregister()
