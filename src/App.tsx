032022da71190babf311223cc8fce1306c836497
Michael Lee <michael.leem42@gmail.com>
2019-03-05 11:13:51 -0800

    added more routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { HashRouter, Route, Switch } from 'react-router-dom'
+import { HashRouter, Route, Switch, Link } from 'react-router-dom'
 import * as React from 'react'
 import './App.css'
 const Home = React.lazy(() => import('./Home'))
@@ -6,27 +6,28 @@ const Explore = React.lazy(() => import('./Explore'))
 const About = React.lazy(() => import('./About'))
 
 const App: React.SFC<{}> = ({}) => (
-  <React.Fragment>
-    <div id="header" className="container center-content">
-      <div className="row">
-        <div className="col-xs-6">
-          CSBC-PSON Data Portal
-        </div>
-        <div className="col-xs-6">
-          Buttons
+  <HashRouter>
+    <React.Fragment>
+      <div id="header" className="container">
+        <div className="row">
+          <div className="col-xs-6 text-center">
+            <Link to="/"> CSBC-PSON Data Portal </Link>
+          </div>
+          <div className="col-xs-6">
+            <Link to="/Explore"> Explore </Link>
+            <Link to="/About"> About </Link>
+          </div>
         </div>
       </div>
-    </div>
-    <HashRouter>
       <React.Suspense fallback={<div>Loading...</div>}>
         <Switch>
           <Route exact={true} path="/" component={Home}/>
-          <Route path="/about" component={About}/>
-          <Route path="/explore" component={Explore}/>
+          <Route path="/About" component={About}/>
+          <Route path="/Explore" component={Explore}/>
         </Switch>
       </React.Suspense>
-    </HashRouter>
-  </React.Fragment>
+    </React.Fragment>
+  </HashRouter >
 )
 
 export default App
