97bacabad012635a54275ed5c81b91ebe7146b10
Michael Lee <michael.leem42@gmail.com>
2019-03-11 09:57:21 -0700

    added spacer below header

--- src/App.tsx
+++ src/App.tsx
@@ -9,25 +9,30 @@ const App: React.SFC<{}> = ({}) => (
   <HashRouter>
     <React.Fragment>
       {/* main links in header */}
-      <div id="header" className="container">
-        <div className="row">
-          <div className="col-xs-6 text-center">
-            <Link to="/"> CSBC-PSON Data Portal </Link>
-          </div>
-          <div className="col-xs-6">
-            <Link to="/Explore/Grants"> Explore </Link>
-            <Link to="/About"> About </Link>
+      <header id="header">
+        <div className="container">
+          <div className="row">
+            <div className="col-xs-6">
+              <Link to="/"> CSBC-PSON Data Portal </Link>
+            </div>
+            <div className="col-xs-6">
+              <Link to="/Explore/Grants"> Explore </Link>
+              <Link to="/About"> About </Link>
+            </div>
           </div>
         </div>
-      </div>
+      </header>
+      <div className="spacer"/>
       {/* all the content below */}
-      <React.Suspense fallback={<div/>}>
-        <Switch>
-          <Route exact={true} path="/" component={Home}/>
-          <Route path="/About" component={About}/>
-          <Route path="/Explore/Grants" component={Explore}/>
-        </Switch>
-      </React.Suspense>
+      <main>
+        <React.Suspense fallback={<div/>}>
+          <Switch>
+              <Route exact={true} path="/" component={Home}/>
+              <Route path="/About" component={About}/>
+              <Route path="/Explore/Grants" component={Explore}/>
+          </Switch>
+        </React.Suspense>
+      </main>
     </React.Fragment>
   </HashRouter >
 )
