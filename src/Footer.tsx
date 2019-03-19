4ec5c038335a6bdb546384aefc13574f0ab53706
Michael Lee <michael.leem42@gmail.com>
2019-03-19 14:36:45 -0700

    fixed spacing in footer

--- src/Footer.tsx
+++ src/Footer.tsx
@@ -6,7 +6,7 @@ export const Footer:React.SFC<{}> = () => {
       <div id="portal-title-footer">
           CSBC - PS/ON
       </div>
-      <div id="portal-contact-footer center-content">
+      <div id="portal-contact-footer" className="center-content">
         <a href="mailto:csbc_pson_dcc@sagebase.org" className="footer-link"> Contact Us </a>
         <a target="_blank" href="https://s3.amazonaws.com/static.synapse.org/governance/SageBionetworksSynapseTermsandConditionsofUse.pdf?v=5" className="footer-link"> Terms of Use </a>
       </div>
